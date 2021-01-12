c2e1575a246550cf8d08ac2f66c8b971aa8422ff
Thiago Falcão da Silva <thiago.falcao86@gmail.com>
2021-01-12 10:01:30 -0300

    Update #2
    Movimentos do helicóptero

--- js/js.js
+++ js/js.js
@@ -8,8 +8,28 @@ function start() { // Inicio da função start()
     $("#fundoGame").append("<div id='amigo' class = 'anima3'></div>");
 
     //Principais variáveis do jogo
-
+    
     var jogo = {}
+    var TECLA = {
+        W: 87,
+        S: 83,
+        D: 68
+    }
+
+    jogo.pressionou = [];
+
+    //Verifica se o usuário pressionou alguma tecla	
+
+    $(document).keydown(function (e) {
+        jogo.pressionou[e.which] = true;
+    });
+
+
+    $(document).keyup(function (e) {
+        jogo.pressionou[e.which] = false;
+    });
+
+    
 
     //Game Loop
 
@@ -18,6 +38,7 @@ function start() { // Inicio da função start()
     function loop() {
 
         movefundo();
+        movejogador();
 
     } // Fim da função loop()
 
@@ -27,5 +48,26 @@ function start() { // Inicio da função start()
         $("#fundoGame").css("background-position", esquerda - 1);
 
     } // fim da função movefundo()
+
+    function movejogador() {
+
+        if (jogo.pressionou[TECLA.W]) {
+            var topo = parseInt($("#jogador").css("top"));
+            $("#jogador").css("top", topo - 10);
+
+        }
+
+        if (jogo.pressionou[TECLA.S]) {
+
+            var topo = parseInt($("#jogador").css("top"));
+            $("#jogador").css("top", topo + 10);
+        }
+
+        if (jogo.pressionou[TECLA.D]) {
+
+            //Chama função Disparo	
+        }
+
+    } // fim da função movejogador()
 
 } // Fim da função start
